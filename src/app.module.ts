f9f12eba1d46e9e6cb9f17792474cbd1ec84d469
ahsan-kwanso <m.ahsan@kwanso.com>
2024-09-09 18:51:13 +0500

    added validation pipes to the app globally

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
+import { APP_PIPE } from '@nestjs/core';
 import { DatabaseModule } from './modules/database.module';
 import { UserModule } from './modules/user/user.module';
 import { PostModule } from './modules/post/post.module';
 import { CommentModule } from './modules/comment/comment.module';
+import { AuthModule } from './modules/auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
@@ -12,6 +14,13 @@ import { ConfigModule } from '@nestjs/config';
     UserModule,
     PostModule,
     CommentModule,
+    AuthModule,
+  ],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useClass: ValidationPipe,
+    },
   ],
 })
 export class AppModule {}
