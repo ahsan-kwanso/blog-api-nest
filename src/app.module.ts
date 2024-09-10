b1172898236b8a50bea923102b1e8a0e3a035f3e
ahsan-kwanso <m.ahsan@kwanso.com>
2024-09-10 10:23:03 +0500

    applied auth middleware to all routes except auth routes for signup and signin, will make it more customizable later

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
-import { Module, ValidationPipe } from '@nestjs/common';
+import {
+  Module,
+  NestModule,
+  ValidationPipe,
+  MiddlewareConsumer,
+  RequestMethod,
+} from '@nestjs/common';
 import { APP_PIPE } from '@nestjs/core';
 import { DatabaseModule } from './modules/database.module';
 import { UserModule } from './modules/user/user.module';
 import { PostModule } from './modules/post/post.module';
 import { CommentModule } from './modules/comment/comment.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { AuthMiddleware } from './modules/auth/auth.middleware';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
@@ -23,4 +30,11 @@ import { ConfigModule } from '@nestjs/config';
     },
   ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthMiddleware)
+      .exclude({ path: 'auth', method: RequestMethod.POST }, 'auth/(.*)') // Exclude auth routes from middleware
+      .forRoutes('*'); // Apply to all routes
+  }
+}
