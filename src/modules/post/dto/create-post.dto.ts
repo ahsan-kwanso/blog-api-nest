1a09976bf1d231acd5ddd595073d01d9b898e8b0
ahsan-kwanso <m.ahsan@kwanso.com>
2024-09-10 15:36:28 +0500

    error messaged added and min max length

--- src/modules/post/dto/create-post.dto.ts
+++ src/modules/post/dto/create-post.dto.ts
@@ -1,12 +1,18 @@
-import { IsString, IsNotEmpty, IsOptional } from 'class-validator';
+import { IsString, IsNotEmpty, IsOptional, Length } from 'class-validator';
 
 export class CreatePostDto {
   @IsString()
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'Title is required' })
+  @Length(1, 25, {
+    message: 'Title must be between 1 and 25 characters long',
+  })
   readonly title: string;
 
   @IsString()
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'Content is required' })
+  @Length(10, 500, {
+    message: 'Content must be between 10 and 500 characters long',
+  })
   readonly content: string;
 
   @IsOptional()
