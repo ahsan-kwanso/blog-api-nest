887fad141d9ca5be02708f2ee0187af47a247442
ahsan-kwanso <m.ahsan@kwanso.com>
2024-09-10 14:59:20 +0500

    increase expiration duration

--- src/utils/jwt.util.ts
+++ src/utils/jwt.util.ts
@@ -2,7 +2,7 @@ import * as jwt from 'jsonwebtoken';
 import { TokenPayload } from 'src/types/tokenPayload.interface';
 
 const SECRET_KEY = process.env.JWT_SECRET || 'your-secret-key';
-const EXPIRATION_TIME = '1h'; // 1 hour
+const EXPIRATION_TIME = '4h'; // 1 hour
 
 export const generateToken = (payload: TokenPayload): string => {
   return jwt.sign(payload, SECRET_KEY, { expiresIn: EXPIRATION_TIME });
